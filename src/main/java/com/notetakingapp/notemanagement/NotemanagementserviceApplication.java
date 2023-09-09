3580225de04eb5474baefe8027172f4410617cbd
Ankit3002 <hmankit2003@gmail.com>
2023-09-09 11:01:43 +0530

    Added: note management service

---
+++ src/main/java/com/notetakingapp/notemanagement/NotemanagementserviceApplication.java
@@ -0,0 +1,13 @@
+package com.notetakingapp.notemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotemanagementserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotemanagementserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/notetakingapp/notemanagement/NotemanagementserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notetakingapp.notemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotemanagementserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
